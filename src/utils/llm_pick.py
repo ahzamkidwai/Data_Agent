0940028baacca09f33e23ff7d163e4636ec8cf20
ahzamkidwai <ahzamnaseem.kidwai@gmail.com>
2026-09-10 02:41:54 +0530

    Update schema & create function for picking LLM

---
+++ src/utils/llm_pick.py
@@ -0,0 +1,28 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def pick_llm(level: str):
+    """Pick the appropriate LLM based on the level of the question.
+
+    Args:
+        level (str): The level of the question, can be "easy", "medium", or "hard".
+        
+    Returns:
+        str: The name of the LLM to be used.
+    """
+    
+    if level.lower() == "low":
+        llm = ChatOpenAI(model='gpt-5.6-luna', temperature=0)
+    elif level.lower() == "medium":
+        llm = ChatOpenAI(model='gpt-5.6-terra', temperature=0)
+    elif level.lower() == "hard":
+        llm = ChatOpenAI(model='gpt-5.6-sol', temperature=0)
+    else:
+        raise ValueError("Invalid level. Chose from 'easy', 'medium', or 'hard'")
+    
+    return llm
+
+llm_obj = pick_llm("low")
+print("LLM OBJECT is : ", llm_obj)
